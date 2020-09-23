1f1d62dddac65ffc7cd9547793616c174087de34
Terranie Sims <terranie67@gmail.com>
2020-09-23 00:25:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
-# Write your code below game_hash
+'require pry'
+
 def game_hash
   {
     home: {
@@ -126,4 +127,7 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored (name)
+  players = game_hash [:home][:players].merge(game_hash[:away][:players])
+end 
+ 
