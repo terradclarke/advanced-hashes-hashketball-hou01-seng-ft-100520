dab05b8991a1921d8ee42bcb25357bd20b5fe5f7
Terranie Sims <terranie67@gmail.com>
2020-09-23 00:45:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,7 +127,7 @@ def game_hash
   }
 end
 
-def number_points_scored(player_name)
+def num_points_scored(player_name)
   game_hash.each do |home_away, keys|
     keys[:players].each do |player|
       return player[:points] if player[:player_name] == player_name 
